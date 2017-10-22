3cf3a1e6961941795054bbf39b1a260d5b77cec7
Kristian Hajdari <ghaidaris@live.com>
2017-10-22 15:43:04 +0200

    Second requirement is done

--- tic-tac-toe/src/main/java/com/tdd/tic_tac_toe/TicTacToe.java
+++ tic-tac-toe/src/main/java/com/tdd/tic_tac_toe/TicTacToe.java
@@ -3,25 +3,34 @@ package com.tdd.tic_tac_toe;
 public class TicTacToe {
 	private Character[][] ticTacToeBoard = { { '\0', '\0', '\0' }, { '\0', '\0', '\0' }, { '\0', '\0', '\0' } };
 
+	private char lastPlayer = '\0';
+
 	public void play(int x, int y) {
 		checkAxis(x);
 		checkAxis(y);
 		setXInBox(x, y);
+		lastPlayer = nextPlayer();
 	}
-	
+
 	public void checkAxis(int axis) {
 		if (axis < 1 || axis > 3) {
 			throw new RuntimeException("X is outside of board");
-		} 
+		}
 	}
-	
+
 	public void setXInBox(int x, int y) {
-		if (ticTacToeBoard[x-1][y-1] != '\0') {
+		if (ticTacToeBoard[x - 1][y - 1] != '\0') {
 			throw new RuntimeException("Box is not free for playing");
+		} else {
+			ticTacToeBoard[x - 1][y - 1] = 'X';
 		}
-		else {
-			ticTacToeBoard[x-1][y-1] = 'X';
+	}
+
+	public char nextPlayer() {
+		if (lastPlayer == 'X') {
+			return 'O';
 		}
+		return 'X';
 	}
 
 	public static void main(String[] args) {
--- tic-tac-toe/src/test/java/com/tdd/tic_tac_toe/TicTacToeSpec.java
+++ tic-tac-toe/src/test/java/com/tdd/tic_tac_toe/TicTacToeSpec.java
@@ -4,6 +4,7 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
+import org.junit.Assert;
 
 /**
  * Unit test for TicTacToe.
@@ -11,6 +12,8 @@ import org.junit.rules.ExpectedException;
 public class TicTacToeSpec {
 	/**
 	 * Specification 1 : A piece can be placed on any empty space of a 3×3 board.
+	 * Specification 2 : There should be a way to find out which player should play
+	 * next.
 	 */
 	@Rule
 	public ExpectedException exception = ExpectedException.none();
@@ -60,5 +63,44 @@ public class TicTacToeSpec {
 		ticTacToe.play(2, 1);
 		exception.expect(RuntimeException.class);
 		ticTacToe.play(2, 1);
+	}
+
+	/**
+	 * Test Requirement for Specification 2 : The first turn should be played by
+	 * played X
+	 *
+	 * @param None
+	 * @return Nothing
+	 */
+	@Test
+	public void givenFirstTurnWhenNextPlayerThenX() {
+		Assert.assertEquals('X', ticTacToe.nextPlayer());
+	}
+
+	/**
+	 * Test Requirement for Specification 2 : If the last turn was played by X, then
+	 * the next turn should be played by O
+	 *
+	 * @param None
+	 * @return Nothing
+	 */
+	@Test
+	public void givenLastTurnWasXWhenNextPlayerThenO() {
+		ticTacToe.play(1, 1);
+		Assert.assertEquals('O', ticTacToe.nextPlayer());
+	}
+
+	/**
+	 * Test Requirement for Specification 2 : If the last turn was played by O, then
+	 * the next turn should be played by X
+	 *
+	 * @param None
+	 * @return Nothing
+	 */
+	@Test
+	public void givenLastTurnWasYWhenNextPlayerThenX() {
+		ticTacToe.play(1, 1);
+		ticTacToe.play(2, 1);
+		Assert.assertEquals('X', ticTacToe.nextPlayer());
 	}
 }
